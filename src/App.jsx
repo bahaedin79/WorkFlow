f330de5304e2264fa3a88b84f96b7246850671e7
Arasaka <bahaeddin365@gmail.com>
2025-07-20 03:54:48 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <div className="w-60 rounded m-5 bg-amber-400 p-4 dark:bg-blue-950">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione eaque esse sint nobis veritatis illum itaque
+          error expedita molestiae quibusdam! Maiores reprehenderit qui officia? Dignissimos eius eligendi nihil.
+          Officia, officiis.
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
